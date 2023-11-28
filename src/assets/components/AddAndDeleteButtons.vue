7a8e2df7ba3bbbe792402eb6358bfb96bba2400d
brianjo94 <brianjo1994@gmail.com>
2023-11-28 13:29:36 -0500

    Created AddAndDelete component and functionality

---
+++ src/assets/components/AddAndDeleteButtons.vue
@@ -0,0 +1,10 @@
+<script setup>
+const emit = defineEmits(['addTemplate', 'removeTemplate'])
+</script>
+
+<template>
+    <div>
+        <button @click="$emit('addTemplate')">Click to add template</button>
+        <button @click="$emit('removeTemplate')">Click to remove template</button>
+  </div>
+</template>
