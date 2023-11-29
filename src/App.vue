750c7fd6af5f31f39933f99262e20663fef3019b
Aya <151888395+Yoyayty@users.noreply.github.com>
2023-11-28 20:57:09 -0400

    move Jayden's  html changes into components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import JSONfile from './assets/questions.json'
 
-import {reactive, ref} from 'vue';
-import QuestionTemplate from './assets/components/questionTemplate.vue';
+import { reactive, ref } from 'vue';
+import QuestionTemplate from './assets/components/QuestionTemplate.vue';
 import AddAndDeleteButtons from './assets/components/AddAndDeleteButtons.vue';
+import Header from './assets/components/layout/Header.vue';
+import Footer from './assets/components/layout/Footer.vue';
 
 const questionsInJSON = reactive(JSONfile)
 
@@ -29,47 +31,63 @@ const startEdit = (questionIndex, question, answer) => {
 </script>
 
 <template>
-  <h1>Quiz</h1>
-  <!-- Outputs the static questions in questions.json-->
-  <div v-for="question in questionsInJSON">
-      <p> {{ question.statement }}</p>
-      <div v-for="(answer, key) in question.answers">
-        <input type="radio"
-                :id ="key"
-                :name="'question-' + question.question">
-        <label :for="key"> {{ answer }}</label>
-      </div>
-   </div>
+  <Header></Header>
+  <!-- Main Content Section -->
+  <main>
+    <!-- Your Quiz Content Goes Here -->
+    <section class="quiz-section">
+      <!-- Quiz questions and blanks go here -->
 
-   <div v-if="editingAvailable">
-    <h2>Edit Question</h2>
-    <form @submit.prevent="saveEdit">
-      <div>
-        <label for="editedQuestion">Question:</label>
-        <input v-model="editedQuestion" id="editedQuestion" type="text" />
+      <h1>Quiz</h1>
+      <!-- Outputs the static questions in questions.json-->
+      <div v-for="question in questionsInJSON">
+        <p> {{ question.statement }}</p>
+        <div v-for="(answer, key) in question.answers">
+          <input type="radio" :id="key" :name="'question-' + question.question">
+          <label :for="key"> {{ answer }}</label>
+        </div>
       </div>
-      <div>
-        <label for="editedAnswer">Answer:</label>
-        <input v-model="editedAnswer" id="editedAnswer" type="text" />
+
+      <div v-if="editingAvailable">
+        <h2>Edit Question</h2>
+        <form @submit.prevent="saveEdit">
+          <div>
+            <label for="editedQuestion">Question:</label>
+            <input v-model="editedQuestion" id="editedQuestion" type="text" />
+          </div>
+          <div>
+            <label for="editedAnswer">Answer:</label>
+            <input v-model="editedAnswer" id="editedAnswer" type="text" />
+          </div>
+          <button type="submit">Save</button>
+        </form>
       </div>
-      <button type="submit">Save</button>
-    </form>
+
+      <!-- Outputs # of question template component based on length of questionsLength -->
+      <QuestionTemplate v-for="i in (questionsLength - 2)" />
+
+
+      <!-- Add or delete buttons to add or subtract total questions -->
+      <AddAndDeleteButtons @removeTemplate="questionsLength--" @addTemplate="questionsLength++" />
+
+
+      <button @click="startEdit(1, 'hi', 'bcd')">
+        Click to edit
+      </button>
+    </section>
+  </main>
+
+  <Footer></Footer>
+  <!-- Hamburger Menu Overlay -->
+  <div class="overlay">
+    <div class="overlay-content">
+      <a href="https://www.adventuretowntoys.com">Home</a>
+      <a href="#">Quiz Categories</a>
+      <a href="https://www.adventuretowntoys.com/about-us">About Us</a>
+    </div>
   </div>
-  
-<!-- Outputs # of question template component based on length of questionsLength -->
-<QuestionTemplate v-for="i in (questionsLength - 2)"/>
-
-
-<!-- Add or delete buttons to add or subtract total questions -->
-<AddAndDeleteButtons @removeTemplate="questionsLength--" @addTemplate="questionsLength++"/>
-
-
-   <button @click="startEdit(1, 'hi', 'bcd')">
-    Click to edit
-  </button>
 </template>
 
 
 
-<style scoped>
-</style>
+<style scoped></style>
