53438f9e2c5661cfd820a14243aab7bbce0d465e
Aya <151888395+Yoyayty@users.noreply.github.com>
2023-11-30 20:18:13 -0400

    added some links to footer

--- src/assets/components/layout/Footer.vue
+++ src/assets/components/layout/Footer.vue
@@ -5,16 +5,30 @@
     <!-- Footer Section -->
     <footer class="site-footer">
         <div class="container">
-            <p>&copy; 2023 Adventuretown Toy Emporium. All Rights Reserved.</p>
+
 
             <div class="site-footer__navigation">
                 <nav class="site-footer__navigation-links nav">
                     <ul>
                         <li>
-                            <a href="https://www.adventuretowntoys.com/customer-account.html" target="_self">My Account</a>
+                            <a href="https://www.adventuretowntoys.com/customer-account.html" target="_self">MY ACCOUNT</a>
                         </li>
                         <li>
-                            <a href="https://www.adventuretowntoys.com/contact-us.html" target="_self">Contact Us</a>
+                            <a href="https://www.adventuretowntoys.com/contact-us.html" target="_self">CONTACT US</a>
+                        </li>
+                        <li>
+                            <a href="https://www.adventuretowntoys.com/shipping-and-return-policy.html" target="_self">
+                                SHIPPING/RETURN POLICY</a>
+                        </li>
+                        <li>
+                            <a href="https://www.adventuretowntoys.com/privacy-policy.html" target="_self"> PRIVACY
+                                POLICY</a>
+                        </li>
+                        <li>
+                            <a href="https://www.adventuretowntoys.com/sitemap.html" target="_self">SITE MAP</a>
+                        </li>
+                        <li>
+                            <a href="https://www.adventuretowntoys.com/news.html" target="_self"> PRESS</a>
                         </li>
                         <!-- Add more footer links as needed -->
                     </ul>
@@ -26,6 +40,7 @@
                         <li>
                             <a class="theme-icon--facebook" href="https://www.facebook.com/AdventuretownToyEmporium/"
                                 target="_blank" rel="noreferrer" aria-label="View our Facebook Page">Facebook</a>
+
                         </li>
                         <li>
                             <a class="theme-icon--instagram" href="https://www.instagram.com/adventuretowntoys/"
@@ -34,6 +49,7 @@
                         <!-- Add more social media links as needed -->
                     </ul>
                 </nav>
+                <p>&copy; 2023 Adventuretown Toy Emporium. All Rights Reserved.</p>
             </div>
         </div>
     </footer>
