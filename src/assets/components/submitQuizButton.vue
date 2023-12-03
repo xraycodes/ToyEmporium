25e0d18b3b4b581facd8fca67853a415120bbb33
kenkbomb <146538181+kenkbomb@users.noreply.github.com>
2023-12-03 00:41:32 -0500

    finalized the submitButton and its popup
    added a dynamic link to the toyemporium website based on which learner type the user got.

--- src/assets/components/submitQuizButton.vue
+++ src/assets/components/submitQuizButton.vue
@@ -1,31 +1,64 @@
 <template>
     <div>
-        <button class = 'subQuizButton' @click="show=!show">Submit Quiz</button>
+        <button class = 'subQuizButton' v-on:click="popUp">Submit Quiz</button>
     </div>
 
-    <div v-show="show" class = 'popUp'>
+    <div v-show="show" class = 'popUpc'>
         <div class = 'popUpHead'>
-     <h1 v v-show="show">Congratulations!</h1>
-     <p> {{learnerType}} </p>
+
+            <img src =  '../images/adventuretown.png' id="logo">
+
+     <h1>Congratulations!</h1>
+     <h2 id = 'lt'>{{learnerType}}</h2>
      <br>
      <p>{{ description }}</p>
         </div>
-        <div class = 'mid'></div>
+        <div class = 'mid'>
+            <a v-bind:href="learnTypeLink">
+                <img src = '../images/factory.gif' class = learnerImg>
+            </a>
+            <p>Check out the factory above for lots of neat things!</p>
+        </div>
     <div class = 'popUpFoot'>
-     <button @click="show = !show" class = 'subQuizButton' id = 'popUpCloseButton'>Close</button>
+     <button @click= "show =!show" class = 'subQuizButton' id = 'popUpCloseButton'>Close</button>
     </div>
     </div>
 </template>
 
+
 <script setup>
 import { ref } from "vue";
 
+
 defineProps({learnerType:String,description:String});
 let show = ref(false);
+let learnTypeLink = '';
+let learnTypeLogo = '';
+let leenteep = '';
+
+// only the keys are used here.
+let links = [{Thinkers:'https://www.adventuretowntoys.com/Thinkers.html'},{Makers:'https://www.adventuretowntoys.com/Makers.html'},{Scientists:'https://www.adventuretowntoys.com/Scientists.html'},{Designers:'https://www.adventuretowntoys.com/Designers.html'},{Fledglings:'https://www.adventuretowntoys.com/Fledglings.html'},{Directors:'https://www.adventuretowntoys.com/Directors.html'}]
+
+
+
+
    function popUp()
    {
-        show = !show;
-        console.log('submit pressed ' + show);
+        show.value = true;
+        console.log('submit pressed ' + show.value);
+        leenteep= document.getElementById('lt').innerText;
+        console.log(leenteep + ' is the learner type');
+        
+        
+        links.forEach(element => {
+            if(leenteep === Object.keys(element).toString())
+            {
+            console.log('MATCH on ' + Object.keys(element) + ' from foreach!');
+            learnTypeLink = 'https://www.adventuretowntoys.com/' + leenteep.toString() +'.html';
+            console.log(learnTypeLink);
+            
+            }
+        });
    }
 
    
@@ -38,25 +71,42 @@ p{
     color: #fff;
     padding-left: 20px;
     padding-right: 20px;
-    
-  font-size: clamp(1rem,2.3vw,2rem);
+    font-size: clamp(0.7rem,1.5vw,1.7rem);
+}
+.learnerImg{
+    width:100%;
+    margin: 0 auto;
+    max-width: fit-content;
+    transition: 1s;
+    min-width: 30vw;
+}
+.learnerImg:hover{
+    transform: scale(1.1);
+    transition: 1s;
 }
 
 h1{
     text-decoration: underline;
-    color: #fff;
+    color: rgb(241, 241, 115);
     text-transform: uppercase;
     margin-bottom: 10px;
-    
-  font-size: clamp(1rem,2.3vw,2rem);
+    font-size: clamp(1rem,1.8vw,2rem);
 }
-
+h2{
+    color: rgb(241, 241, 115);
+    text-transform: uppercase;
+    margin-bottom: 5px;
+}
+#logo{
+    width:100%;
+    margin: 0 auto;
+    max-width: 40%;
+}
 .mid{
-    height:50vh;
+    height:100vh;
 }
 .popUpHead{
     border-radius: 2px;
-    background-color:rgb(15, 150, 82) ;
     padding: 10px;
     border: solid 5px #09715c solid;
 }
@@ -64,18 +114,14 @@ h1{
     background-color: #09715c solid;
 }
 #popUpCloseButton{
-    
-   /* position: relative; */
-   /* top:65vh; */
-   width: 100%;
+    width: 100%;
    margin: 0 auto;
    border: solid 1px rgb(15, 150, 82) ;
-    
 }
-.popUp{
+.popUpc{
     width: 60vw;
-    height: 80%;
-    background-color: rgba(15, 150, 82, 0.411);
+    height: 90%;
+    background-color: rgba(15, 150, 82, 0.884);
     position: fixed;
     top:15px;
     border-radius: 5px;
@@ -83,11 +129,10 @@ h1{
     backdrop-filter: blur(4px);
     display: flex;
     flex-direction: column;
-    margin-left: 20vw;
-   
-
-
-}
+     margin-left: 16vw; 
+     margin-right:19vw;
+     
+    }
 
 .subQuizButton{
     appearance: none;
